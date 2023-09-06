63ae74ac483f4c2113803332258ea866f625909e
OksanaSokal <sokaloksana@gmail.com>
2023-09-06 11:32:35 +0300

    feat: add user card

--- library/script.js
+++ library/script.js
@@ -202,7 +202,6 @@ sliderBtnPrev.addEventListener('click', prevSlide);
     //модальное окно
 
     const iconBtn = document.getElementById('open__modal-btn');
-    // const modalBox = document.querySelector('.modal__box');
 
     iconBtn.addEventListener('click', function() {
         document.getElementById('my__modal').classList.add('open');
@@ -215,8 +214,10 @@ sliderBtnPrev.addEventListener('click', prevSlide);
     })
 
     document.getElementById('my__modal').addEventListener('click', event => {
-        // if (event._isClickWithInModalReg) return;
+        if (event._isClickWithInModalReg) return;
         if (event._isClickWithInModal) return;
+        if (event._isClickWithInModalProf) return;
+        if(event._isClickWithInModalCard) return;
         event.currentTarget.classList.remove('open');
     })
   
@@ -230,9 +231,10 @@ sliderBtnPrev.addEventListener('click', prevSlide);
         document.querySelector('.modal__box').style.display = 'none';
     })
 
-    // if (!document.getElementById('registration').classList.contains('open_reg')) {
-    //     document.querySelector('.modal__box').style.display = 'block';
-    // } 
+    document.getElementById('close__modal').addEventListener('click', function(){
+        document.getElementById('registration').classList.remove('open_reg');
+        document.getElementById('my__modal').classList.remove('open');
+    })
 
 
 
@@ -247,7 +249,154 @@ sliderBtnPrev.addEventListener('click', prevSlide);
     })
 
 
-    //local storage
+    /////////local storage
+
+    let data = localStorage.getItem('usersList');
+    let usersList = [];
+
+    if(data !== '' && data !== null) {
+        usersList = JSON.parse(data);
+       
+    }
+
+
+    //функция создать модал окно profile
+
+    const createProfileWindow = function() {
+        const profileBtn = document.querySelector('.user__letters');
+    
+        profileBtn.addEventListener('click', function(){
+           document.getElementById('my__modal').classList.add('open');
+           document.getElementById('profile__modal').classList.add('open__prof');
+           document.querySelector('.modal__box').style.display = 'none';
+           document.querySelector('.modal__reg').style.display = 'none';
+           document.querySelector('.modal__card').style.display = 'none';
+          document.querySelector('.modal__profile').style.display = 'block';
+           })
+
+
+           document.querySelector('#my__modal .profile__box').addEventListener('click', event => {
+               event._isClickWithInModalProf = true;
+           })
+       
+       
+           document.getElementById('profile__modal').addEventListener('click', event => {
+               if(event._isClickWithInModalProf) return;
+               event.currentTarget.classList.remove('open__prof');
+           })
+
+    }
+
+    //функция создать инициалы для карты профиля
+
+        const createInitialsforProfile = function() {
+            let blockForLetters = document.querySelector('.card__initials');
+
+        }
+
+
+    
+
+    // btnCopy.addEventListener('click', function(){
+    //     let copyText = document.getElementById('usernumber');
+    //     copyText.select();
+    //     document.execCommand('copy');
+    // })
+    
+
+
+         //функция создать модал окно profile -card
+
+         const createCardModal = function() {
+            const profileCardBtn = document.querySelector('.profile__link');
+
+            profileCardBtn.addEventListener('click', function(){
+                document.getElementById('my__modal').classList.add('open');
+                document.getElementById('card__modal').classList.add('open__card');
+                document.querySelector('.modal__box').style.display = 'none';
+                document.querySelector('.modal__reg').style.display = 'none';
+                document.querySelector('.modal__profile').style.display = 'none';
+                document.querySelector('.modal__card').style.display = 'flex';
+                })
+
+            document.getElementById('close__profile__modal').addEventListener('click', function(){
+                document.getElementById('card__modal').classList.remove('open__card');
+                document.getElementById('my__modal').classList.remove('open');
+            })
+     
+     
+                document.querySelector('#my__modal .card__box').addEventListener('click', event => {
+                    event._isClickWithInModalCard = true;
+                })
+            
+            
+                document.getElementById('card__modal').addEventListener('click', event => {
+                    if(event._isClickWithInModalCard) return;
+                    event.currentTarget.classList.remove('open__card');
+                })
+         }
+
+
+
+
+
+    usersList.forEach((item) => {
+        if (item.status == 'active') {
+
+
+            // вставка букв вместо иконки
+            let firstNameUser = item.firstname;
+            console.log(firstNameUser)
+            let lastNameUser = item.lastname;
+
+            let userLetters = firstNameUser[0] + lastNameUser[0];
+        
+            let lettersBlock = document.createElement('span');
+            lettersBlock.classList.add('user__letters');
+
+            lettersBlock.textContent = userLetters;
+            
+            document.querySelector('.header-box').append(lettersBlock)
+            document.querySelector('.header__img').style.display = 'none';
+            lettersBlock.title = `${firstNameUser} ${lastNameUser}`;
+
+            //вставка букв и имени в карту профиля 
+            let blockForLetters = document.querySelector('.card__initials');
+            blockForLetters.textContent = userLetters;
+
+            let blockForName = document.querySelector('.card__name');
+            blockForName.textContent = `${firstNameUser} ${lastNameUser}`;
+
+            //модальное окно profile
+            createProfileWindow();
+
+            //вставка в профиль номера карты 
+
+            let profileNumber = document.querySelector('.profile__number');
+            let userNumber = item.cardnumber;
+            profileNumber.textContent =  userNumber;
+
+            let profileNumberInCard = document.querySelector('.card__usernumber');
+            profileNumberInCard.textContent =  userNumber;
+
+
+
+            //модальное окно профиль юзера
+            createCardModal();
+
+
+            //функция скопировать номер карты в буфер обмена
+            const btnCopy = document.querySelector('.card__copy');
+
+            btnCopy.onclick = () => {
+                navigator.clipboard.writeText(userNumber);
+    }
+
+         }
+      })
+        
+
+    //   Создание Юзера
 
     let firstname = document.querySelector('#firstname');
     let lastname = document.querySelector('#lastname');
@@ -255,18 +404,27 @@ sliderBtnPrev.addEventListener('click', prevSlide);
     let password = document.querySelector('#password');
     let submit = document.querySelector('#submit');
 
-    let users = {};
+    // let users = {};
 
-    function User(firstname, lastname, email, password) {
+    function User(firstname, lastname, email, password, cardnumber, status) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
         this.password = password;
+        this.cardnumber = cardnumber;
+        this.status = status;
     }
 
 
-    function createId(users) {
-        return Object.keys(users).length;
+    // function createId(users) {
+    //     return Object.keys(users).length;
+    // }
+
+    function createCardnumber(){
+        let userNumber = Math.round(Math.random() * 10000000000);
+        userNumber = userNumber.toString(16);
+        userNumber =`F${userNumber.toUpperCase()}`
+         return userNumber;
     }
 
     submit.addEventListener('click', function() {
@@ -274,12 +432,101 @@ sliderBtnPrev.addEventListener('click', prevSlide);
         const lastNameUser = lastname.value;
         const emailUser = email.value;
         const passwordUser = password.value;
+        const cardNumber = createCardnumber();
+        const status = 'active';
 
-        const user = new User(firstNameUser, lastNameUser, emailUser, passwordUser);
+        const user = new User(firstNameUser, lastNameUser, emailUser, passwordUser, cardNumber, status);
 
-        const userId = 'User' + createId(users);
-        users[userId] = user;
+        // const userId = 'User' + createId(users);
+        // users[userId] = user;
 
-        console.log(user);
+        usersList.push(user);
+    
+        localStorage.setItem('usersList', JSON.stringify(usersList));
 
+    })
+
+   
+
+    //валидация
+    
+    function validation(form) {
+
+        function removeError(input){
+            const parent = input.parentNode;
+
+            if(parent.classList.contains('error')){
+                parent.querySelector('.error__label').remove()
+                parent.classList.remove('error')
+            }
+        }
+
+        function createError(input, text) {
+            const parent = input.parentNode;
+            const errorLabel = document.createElement('label')
+
+            errorLabel.classList.add('error__label');
+            errorLabel.textContent = text;
+
+            parent.classList.add('error');
+            parent.append(errorLabel);
+        }
+
+        let result = true;
+
+        const allInputs = form.querySelectorAll('input')
+
+
+        for (const input of allInputs){
+
+            removeError(input)
+
+            if(input.dataset.maxLength) {
+                
+                if(input.value.length < input.dataset.maxLength){
+                    removeError(input)
+                    createError(input, `Минимальное количество символов: ${input.dataset.maxLength}`)
+                    result = false;
+                }
+            }
+
+            if(input.value == '') {
+                removeError(input)
+                createError(input, 'Поле не заполнено!')
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+
+    document.getElementById('add__form').addEventListener('submit', function(event){
+        event.preventDefault();
+
+        if (validation(this)== true) {
+            alert('Регистрация прошла успешно!')
+
+            //попытка изменить иконку
+            // let firstNameUser = document.querySelector('#firstname').value;
+            // let lastNameUser = document.querySelector('#lastname').value;
+
+            // let userLetters = firstNameUser[0] + lastNameUser[0];
+        
+
+            // let lettersBlock = document.createElement('span');
+            // lettersBlock.classList.add('user__letters');
+
+            // lettersBlock.textContent = userLetters;
+            
+            // document.querySelector('.header-box').append(lettersBlock)
+
+            // document.querySelector('.header__img').style.display = 'none';
+
+            // lettersBlock.title = `${firstNameUser} ${lastNameUser}`;
+
+            // localStorage.setItem('usersList', JSON.stringify(usersList));
+
+
+        }
     })
