3964b71920f9ea747a7b0821df38e33194fdadab
OksanaSokal <sokaloksana@gmail.com>
2023-09-29 20:49:07 +0300

    feat: add function for api

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,24 +1,73 @@
-const url =
-  'https://api.unsplash.com/search/photos?query=monochromatic&client_id=ExzoYddcdJAlyvSfzzfD9onYq9zVqp77J7iQY4-lvfA';
-const container = document.querySelector('.container');
+const container = document.querySelector('.container'),
+  input = document.querySelector('.header__input'),
+  form = document.querySelector('form');
 
-function showImage(address) {
-  image.src = `${address}`;
-}
-console.log(image);
+const apiKey = 'ExzoYddcdJAlyvSfzzfD9onYq9zVqp77J7iQY4-lvfA';
 
-async function getData() {
-  const res = await fetch(url);
+// const url = `https://api.unsplash.com/photos/?per_page=15&client_id=${apiKey}`;
+const url = `https://api.unsplash.com/search/photos?query=monochrome&per_page=15&client_id=${apiKey}`;
+
+// const url = `https://api.unsplash.com/search/photos?query=${request}&per_page=15&client_id=${apiKey}`;
+
+getData(url);
+
+async function getData(dataLink) {
+  const res = await fetch(dataLink);
   const data = await res.json();
   console.log(data);
-  showImage();
-}
-getData();
 
-function createImage() {
-  const img = document.createElement('img');
-  img.classList.add('gallery__image');
-  img.src = ``;
-  img.alt = 'image';
-  container.append(img);
+  const newData = data.results;
+
+  newData.map((item) => {
+    createImage(item.urls.regular);
+  });
 }
+
+function createImage(address) {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+
+  div.classList.add('box__image');
+  img.classList.add('gallery__image');
+  img.src = `${address}`;
+  img.alt = 'image';
+  div.append(img);
+  container.append(div);
+}
+
+// async function getDataSearch() {
+//   const res = await fetch(searchUrl);
+//   const data = await res.json();
+//   console.log(data);
+
+//   data.map((item) => {
+
+//     createImage(item.regulars.urls.regular);
+//   });
+// }
+
+form.addEventListener('submit', (elem) => {
+  elem.preventDefault();
+
+  let request = input.value;
+  const searchUrl = `https://api.unsplash.com/search/photos?per_page=15&query=${request}&client_id=${apiKey}`;
+
+  if (input.value) {
+    container.innerHTML = '';
+    getData(searchUrl);
+  }
+});
+
+const searchBtn = document.querySelector('.button');
+
+searchBtn.addEventListener('click', () => {
+  elem.preventDefault();
+
+  let request = input.value;
+  const searchUrl = `https://api.unsplash.com/search/photos?per_page=15&query=${request}&client_id=${apiKey}`;
+
+  if (input.value) {
+    container.innerHTML = '';
+    getData(searchUrl);
+  }
+});
